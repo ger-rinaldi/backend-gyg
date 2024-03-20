6b314308758014366070d12d4ddc1e932c96c2f9
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-03-19 23:02:23 -0300

    fix: make user username and email DB fields unique

--- presupuestador_gyg_api/models/user.py
+++ presupuestador_gyg_api/models/user.py
@@ -9,6 +9,6 @@ class User(CustomBase):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(40))
     surname: Mapped[str] = mapped_column(String(40))
-    username: Mapped[str] = mapped_column(String(20))
-    email: Mapped[str] = mapped_column(String(250))
+    username: Mapped[str] = mapped_column(String(20), unique=True)
+    email: Mapped[str] = mapped_column(String(250), unique=True)
     password_hash: Mapped[str] = mapped_column(String(60))
