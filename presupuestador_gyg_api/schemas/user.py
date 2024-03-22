c6cd168d98808032f82b4b7711fb56976f6e0875
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-03-22 04:13:07 -0300

    fix: add fields validation to user schema

--- presupuestador_gyg_api/schemas/user.py
+++ presupuestador_gyg_api/schemas/user.py
@@ -1,10 +1,14 @@
-from pydantic import BaseModel, SecretStr
+from pydantic import (
+    BaseModel,
+    EmailStr,
+    Field,
+)
 
 
 class User(BaseModel):
-    id: int
-    name: str
-    surname: str
-    username: str
-    email: str
-    password: SecretStr
+    name: str = Field(min_length=2, max_length=40)
+    surname: str = Field(min_length=2, max_length=40)
+    username: str = Field(min_length=2, max_length=20)
+    email: EmailStr
+    password: Field(min_length=10, max_length=40)  # type: ignore
+    is_admin: bool = Field(default=False)
